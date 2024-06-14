46ffed9018704d30acec4474a835ea0a4050f776
unknown <faridatn099@gmail.com>
2024-06-14 14:10:27 +0700

    fixing

---
+++ src/components/GroupComponent1.jsx
@@ -0,0 +1,58 @@
+import { useMemo } from "react";
+import PropTypes from "prop-types";
+
+const GroupComponent1 = ({ className = "", propTop }) => {
+  const groupDivStyle = useMemo(() => {
+    return {
+      top: propTop,
+    };
+  }, [propTop]);
+
+  return (
+    <div
+      className={`absolute top-[1312px] left-[85px] w-[804px] h-[241px] text-left text-xl text-black font-inria-sans ${className}`}
+      style={groupDivStyle}
+    >
+      <div className="absolute top-[0px] left-[0px] flex flex-row items-start justify-start gap-[10px]">
+        <div className="w-[67px] relative rounded-[50%] bg-gainsboro h-[67px] z-[0]" />
+        <img
+          className="w-[41px] absolute !m-[0] top-[13px] left-[13px] h-[41px] object-cover z-[1]"
+          alt=""
+          src="/image-36@2x.png"
+        />
+      </div>
+      <div className="absolute top-[9px] left-[87px] flex flex-col items-start justify-start gap-[18px]">
+        <div className="rounded-3xs bg-gainsboro overflow-hidden flex flex-col items-start justify-start py-0.5 px-[5px]">
+          <div className="flex flex-row items-center justify-center p-2.5">
+            <b className="relative">nama@gmail.com</b>
+          </div>
+        </div>
+        <div className="rounded-3xs bg-gainsboro overflow-hidden flex flex-col items-start justify-start py-3.5 px-2.5">
+          <div className="flex flex-row items-start justify-start gap-[8px]">
+            <img
+              className="w-[26px] relative h-6 object-cover"
+              alt=""
+              src="/image-37@2x.png"
+            />
+            <b className="relative">5.0</b>
+          </div>
+        </div>
+        <div className="rounded-3xs bg-gainsboro overflow-hidden flex flex-row items-center justify-center py-6 px-[23px]">
+          <b className="w-[671px] relative inline-block shrink-0">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua
+          </b>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+GroupComponent1.propTypes = {
+  className: PropTypes.string,
+
+  /** Style props */
+  propTop: PropTypes.any,
+};
+
+export default GroupComponent1;
--- src/pages/DashboardPenyewa.js
+++ src/pages/DashboardPenyewa.js
@@ -1,160 +1,93 @@
 import "antd/dist/antd.min.css";
 import { Button } from "antd";
+import GroupComponent1 from "../components/GroupComponent1";
+import GroupComponent from "../components/GroupComponent";
 import Header from "../components/Header";
-import "./DashboardPenyewa.css";
 
 const DashboardPenyewa = () => {
   return (
-    <div className="dashboard-penyewa">
-      <div className="dashboard-penyewa-child" />
-      <div className="dashboard-penyewa-inner">
-        <div className="group-child" />
+    <div className="w-full relative bg-whitesmoke-100 h-[2407px] overflow-hidden text-left text-11xl text-black font-inria-sans">
+      <div className="absolute top-[0px] left-[-5px] bg-gainsboro w-[1440px] h-[164px]" />
+      <div className="absolute top-[146px] left-[1760px] w-[427px] h-[249px]">
+        <div className="absolute top-[0px] left-[0px] rounded-3xs bg-dimgray w-[427px] h-[249px]" />
       </div>
-      <img className="dashboard-penyewa-item" alt="" src="/group-2552.svg" />
-      <div className="frame-div" />
+      <img
+        className="absolute h-[2.54%] w-[3.47%] top-[4.95%] right-[6.6%] bottom-[92.51%] left-[89.93%] max-w-full overflow-hidden max-h-full"
+        alt=""
+        src="/group-2552.svg"
+      />
+      <div className="absolute top-[164px] left-[66px] w-[167px] h-[67px]" />
       <Button
-        className="group-button"
+        className="absolute top-[164px] right-[88px]"
         style={{ width: "167px" }}
         type="default"
       >
         kamar saya
       </Button>
       <Button
-        className="dashboard-penyewa-child1"
+        className="absolute top-[1199px] right-[103px]"
         style={{ width: "111px" }}
         type="default"
       >
         Nilai
       </Button>
       <Button
-        className="dashboard-penyewa-child2"
+        className="absolute top-[2285px] right-[96px]"
         style={{ width: "214px" }}
         type="default"
       >
         Lihat Daftar Kost
       </Button>
-      <img className="group-icon" alt="" src="/group-2553@2x.png" />
-      <div className="investasi-rumah-kos-dengan-car-parent">
-        <img
-          className="investasi-rumah-kos-dengan-car"
-          alt=""
-          src="/investasi-rumah-kos-dengan-cara-pintar-gan---kaskus-1@2x.png"
-        />
-        <img className="download-1-icon" alt="" src="/download-1@2x.png" />
+      <img
+        className="absolute top-[255px] left-[334px] w-[771px] h-[508px] object-cover"
+        alt=""
+        src="/group-2553@2x.png"
+      />
+      <div className="absolute top-[1064px] left-[93px] w-6 h-6">
+        <div className="absolute top-[0px] left-[0px] w-6 h-6" />
       </div>
-      <div className="electrical-storm-weather-symbo-wrapper">
-        <div className="electrical-storm-weather-symbo" />
-      </div>
-      <div className="kost-herman-parent">
-        <b className="kost-herman">Kost Herman</b>
-        <div className="jlmanunggal-no169-gn">
+      <div className="absolute top-[791px] left-[85px] w-[650px] h-[108px] text-16xl">
+        <b className="absolute top-[0px] left-[0px]">Kost Herman</b>
+        <div className="absolute top-[36px] left-[0px] text-11xl font-light inline-block w-[650px]">
           Jl.Manunggal No.169, Gn. Bahagia, Kecamatan Balikpapan Selatan, Kota
           balikpapan
         </div>
       </div>
-      <div className="review-parent">
-        <b className="review">5.0 (3 Review)</b>
-        <img className="image-32-icon" alt="" src="/image-32@2x.png" />
+      <div className="absolute top-[1216px] left-[88px] w-[216px] h-9">
+        <b className="absolute top-[0px] left-[35px]">5.0 (3 Review)</b>
+        <img
+          className="absolute top-[4px] left-[0px] w-[27px] h-[25px] object-cover"
+          alt=""
+          src="/image-32@2x.png"
+        />
       </div>
-      <div className="frame-parent">
-        <div className="ellipse-parent">
-          <div className="frame-child" />
-          <img className="image-36-icon" alt="" src="/image-36@2x.png" />
-        </div>
-        <div className="frame-group">
-          <div className="frame-wrapper">
-            <div className="namagmailcom-wrapper">
-              <b className="namagmailcom">nama@gmail.com</b>
-            </div>
-          </div>
-          <div className="frame-container">
-            <div className="image-37-parent">
-              <img className="image-37-icon" alt="" src="/image-37@2x.png" />
-              <b className="namagmailcom">5.0</b>
-            </div>
-          </div>
-          <div className="lorem-ipsum-dolor-sit-amet-co-wrapper">
-            <b className="lorem-ipsum-dolor">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua
-            </b>
-          </div>
-        </div>
+      <GroupComponent1 />
+      <div className="absolute top-[42px] left-[1283px] flex flex-row items-start justify-start gap-[10px]">
+        <div className="w-[67px] relative rounded-[50%] bg-whitesmoke-200 h-[67px] z-[0]" />
+        <img
+          className="w-[41px] absolute !m-[0] top-[13px] left-[13px] h-[41px] object-cover z-[1]"
+          alt=""
+          src="/image-36@2x.png"
+        />
       </div>
-      <div className="ellipse-group">
-        <div className="frame-item" />
-        <img className="image-36-icon" alt="" src="/image-36@2x.png" />
-      </div>
-      <div className="rectangle-parent">
-        <div className="group-item" />
-        <div className="ellipse-container">
-          <div className="frame-item" />
-          <img className="image-36-icon" alt="" src="/image-36@2x.png" />
+      <div className="absolute top-[0px] left-[-5px] w-[1440px] h-[164px]">
+        <div className="absolute top-[0px] left-[0px] bg-gainsboro w-[1440px] h-[164px]" />
+        <div className="absolute top-[42px] left-[1288px] flex flex-row items-start justify-start gap-[10px]">
+          <div className="w-[67px] relative rounded-[50%] bg-whitesmoke-200 h-[67px] z-[0]" />
+          <img
+            className="w-[41px] absolute !m-[0] top-[13px] left-[13px] h-[41px] object-cover z-[1]"
+            alt=""
+            src="/image-36@2x.png"
+          />
         </div>
       </div>
-      <div className="group-div">
-        <div className="ellipse-parent">
-          <div className="frame-child" />
-          <img className="image-36-icon" alt="" src="/image-36@2x.png" />
-        </div>
-        <div className="frame-group">
-          <div className="frame-wrapper">
-            <div className="namagmailcom-wrapper">
-              <b className="namagmailcom">nama@gmail.com</b>
-            </div>
-          </div>
-          <div className="frame-container">
-            <div className="image-37-parent">
-              <img className="image-37-icon" alt="" src="/image-37@2x.png" />
-              <b className="namagmailcom">5.0</b>
-            </div>
-          </div>
-          <div className="lorem-ipsum-dolor-sit-amet-co-wrapper">
-            <b className="lorem-ipsum-dolor">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua
-            </b>
-          </div>
-        </div>
-      </div>
-      <div className="frame-parent2">
-        <div className="ellipse-parent">
-          <div className="frame-child" />
-          <img className="image-36-icon" alt="" src="/image-36@2x.png" />
-        </div>
-        <div className="frame-group">
-          <div className="frame-wrapper">
-            <div className="namagmailcom-wrapper">
-              <b className="namagmailcom">nama@gmail.com</b>
-            </div>
-          </div>
-          <div className="frame-container">
-            <div className="image-37-parent">
-              <img className="image-37-icon" alt="" src="/image-37@2x.png" />
-              <b className="namagmailcom">5.0</b>
-            </div>
-          </div>
-          <div className="lorem-ipsum-dolor-sit-amet-co-wrapper">
-            <b className="lorem-ipsum-dolor">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua
-            </b>
-          </div>
-        </div>
-      </div>
-      <div className="group-parent">
-        <div className="rectangle-group">
-          <div className="group-inner" />
-          <div className="frame-wrapper5">
-            <div className="deskripsi-kost-parent">
-              <b className="namagmailcom">Deskripsi Kost</b>
-              <div className="frame-child2" />
-            </div>
-          </div>
-        </div>
-        <div className="lorem-ipsum-dolor-container">
-          <p className="erat-et-lobortis">{`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vulputate condimentum `}</p>
-          <p className="erat-et-lobortis">
+      <GroupComponent1 propTop="1632px" />
+      <GroupComponent1 propTop="1952px" />
+      <div className="absolute top-[927px] left-[85px] w-[1260px] h-[229px]">
+        <GroupComponent />
+        <div className="absolute top-[85px] left-[4px]">
+          <p className="m-0">{`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vulputate condimentum `}</p>
+          <p className="m-0">
             erat, et lobortis magna suscipit sit amet. Suspendisse malesuada et
             ligula nec porttitor. 
           </p>
