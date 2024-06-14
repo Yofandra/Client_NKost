8dfdbabf0a12aa80480c49cccb1d41cd630519ab
Yofandra <drayofan@gmail.com>
2024-06-14 22:07:39 +0700

    fix deployment 2

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 // import App from './App.jsx'
-import './main.css'
+import "./main.css";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import ForgotPassword from "./pages/ForgotPassword";
@@ -11,18 +11,15 @@ import DashboardPenyewa from "./pages/penyewa/DashboardPenyewa";
 import Navbar from "./components/Navbar";
 import Kamar from "./pages/penyewa/Kamar";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <Router>
-      <Navbar/>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/dashboard_penyewa" element={<DashboardPenyewa />} />
-        <Route path="/kamar" element={<Kamar />} />
-      </Routes>
-    </Router>
-)
-
-reportWebVitals();
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/penyewa/dashboard" element={<DashboardPenyewa />} />
+      <Route path="/penyewa/kamar" element={<Kamar />} />
+    </Routes>
+  </Router>
+);
