3d6294781f5abed1214e516c78553e9117501afc
Yofandra <drayofan@gmail.com>
2024-06-14 21:50:50 +0700

    fix deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import "./global.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import App from './App.jsx'
+import './main.css'
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import ForgotPassword from "./pages/ForgotPassword";
+import Profile from "./pages/Profile";
+import DashboardPenyewa from "./pages/penyewa/DashboardPenyewa";
+import Navbar from "./components/Navbar";
+import Kamar from "./pages/penyewa/Kamar";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+      <Navbar/>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/dashboard_penyewa" element={<DashboardPenyewa />} />
+        <Route path="/kamar" element={<Kamar />} />
+      </Routes>
+    </Router>
 )
 
 reportWebVitals();
