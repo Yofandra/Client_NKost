582042209ea624d5c270b3ec2bd001776bd90ccb
Yofandra <drayofan@gmail.com>
2024-06-19 00:31:39 +0700

    fix navbar penyewa, page kamar, & page laporan

--- src/pages/penyewa/PageLaporan.jsx
+++ src/pages/penyewa/PageLaporan.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import leftArrow from "../../assets/images/left-arrow.png";
-import Navbar from "../../components/Navbar";
+import Navbar from "../../components/NavbarPenyewa";
 import FormLaporan from "../../components/FormLaporan";
 
 const PageLaporan = () => {
@@ -17,12 +17,13 @@ const PageLaporan = () => {
         </div>
         <FormLaporan />
         <form className="flex flex-col bg-[#D9D9D9] bg-opacity-30 w-full p-3 my-10 text-black">
-            <label className="bg-transparent font-bold mb-2">Buat Laporan</label>
-            <input className=" bg-[#D9D9D9] rounded-md py-3 px-1">
-            </input>
-            <div className="flex justify-end my-6 bg-transparent">
-                <button className="bg-[#F39200] hover:hover:bg-[#d37f00] w-32">Tambah</button>
-            </div>
+          <label className="bg-transparent font-bold mb-2">Buat Laporan</label>
+          <input className=" bg-[#D9D9D9] rounded-md py-3 px-1"></input>
+          <div className="flex justify-end my-6 bg-transparent">
+            <button className="bg-[#F39200] hover:hover:bg-[#d37f00] w-32">
+              Tambah
+            </button>
+          </div>
         </form>
       </div>
     </>
