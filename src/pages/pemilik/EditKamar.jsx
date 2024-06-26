b6c01fe24cc7ff35c1bdf7b63ab11c7662663522
Yofandra <drayofan@gmail.com>
2024-06-26 13:50:47 +0700

    fix navigate 2

--- src/pages/pemilik/EditKamar.jsx
+++ src/pages/pemilik/EditKamar.jsx
@@ -72,7 +72,7 @@ const EditKamar = () => {
                 }
             });
 
-            navigate(`/pemilik/kelola-kamar/${response.data.id_kost}`);
+            navigate(`/pemilik/edit-kamar/${id}`);
         } catch (error) {
             console.error('Error updating room:', error);
         }
